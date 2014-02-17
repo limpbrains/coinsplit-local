11c753533d49c57ac83b201e196bfbae97a6e855
Ivan Vershigora <limpbrains@mail.ru>
2014-02-17 13:44:24 +0400

    grunt-node-webkit-builder

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,26 @@ module.exports = function(grunt) {
         src:      'partials/**.html',
         dest:     '_public/templates.js'
       }
+    },
+    nodewebkit: {
+      options: {
+        // version: "0.9.1",
+        build_dir: '../dist',
+        // specifiy what to build
+        mac: false,
+        win: false,
+        // linux32: true,
+        linux64: true,
+        keep_nw: true
+      },
+      src: [
+        './**/*'
+      ]
     }
   });
 
-
-
   grunt.loadNpmTasks('grunt-angular-templates');
+  grunt.loadNpmTasks('grunt-node-webkit-builder');
 
   grunt.registerTask('default', ['ngtemplates']);
 };
